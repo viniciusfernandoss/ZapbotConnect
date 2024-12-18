8e5897260d1d2502765e48ea04ba1bb72d4170ec
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-12-18 13:58:27 -0300

    inicial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//===================================================//
+//                      Navbar                       //
+//===================================================//
+// window.onscroll = function() {
+//     var containerH = document.getElementById('headerr');
+//     if (window.scrollY > 1) { // Ajuste este valor conforme necessário
+//         containerH.style.display = 'none';
+//     } else {
+//         containerH.style.display = 'block';
+//     }
+// };
