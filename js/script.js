7a68294f43a7a59738ae53e26098785b1745958a
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-12-26 18:49:31 -0300

    Trabalho 26/12/2024

--- js/script.js
+++ js/script.js
@@ -9,3 +9,51 @@
 //         containerH.style.display = 'block';
 //     }
 // };
+
+const swiperOne = new Swiper('.swiper-chamado-mobile', {
+    breakpoints: {
+             0: {
+                 slidesPerView: 1
+             },
+             992: {
+                 slidesPerView: 2
+             },
+             1024: {
+                 slidesPerView: 3
+             }
+    },
+    // navigation: {
+    //         nextEl: '.swiper-case-sucesso-mobile .swiper-button-next',
+    //         prevEl: '.swiper-case-sucesso-mobile .swiper-button-prev',
+    // },
+    spaceBetween: 30, // Espaço entre os slides
+    loop: true,
+    pagination: {
+      el: '.swiper-chamado-mobile .swiper-pagination',
+      clickable: true,
+    },
+});
+
+const swiperTwo = new Swiper('.swiper-vantagens-mobile', {
+    breakpoints: {
+             0: {
+                 slidesPerView: 1
+             },
+             992: {
+                 slidesPerView: 2
+             },
+             1024: {
+                 slidesPerView: 3
+             }
+    },
+    // navigation: {
+    //         nextEl: '.swiper-case-sucesso-mobile .swiper-button-next',
+    //         prevEl: '.swiper-case-sucesso-mobile .swiper-button-prev',
+    // },
+    spaceBetween: 30, // Espaço entre os slides
+    loop: true,
+    pagination: {
+      el: '.swiper-vantagens-mobile .swiper-pagination',
+      clickable: true,
+    },
+});
